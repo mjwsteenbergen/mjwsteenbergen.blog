36d19f60ea4b90fb4635b91319e13bc08a3f09c8
Martijn Steenbergen <m.uses+git@outlook.com>
2020-11-09 22:16:36 +0100

    Add about me

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -27,10 +27,20 @@ const StaticPropsDetail = ({ item, errors }: Props) => {
     >
       <img src={item?.coverImage} className="coverimage"></img>
       <div className="article">
+        <p className="back-to-main">By Martijn Steenbergen</p>
         <h1 className="article-title">{item?.name}</h1>
-        <article dangerouslySetInnerHTML={{ __html: item?.html || "" }} >
-
-        </article>
+        <article dangerouslySetInnerHTML={{ __html: item?.html || "" }} ></article>
+      </div>
+      <div className="about-box">
+        <img src="https://avatars0.githubusercontent.com/u/5946409"></img>
+        <div className="about-box-box">
+          <p>Hi! I'm Martijn Steenbergen. I am a 23 year old student living in Delft, where I have been studying Computer Science for 5 years. I have traveled the world, am fascinated with UI/UX and love TV-series. </p>
+          <div className="icon-container">
+            <a href="https://github.com/mjwsteenbergen/"><img src="/icons/logo-github.svg"></img></a>
+            <a href="https://www.linkedin.com/in/mjwsteenbergen/"><img src="/icons/logo-linkedin.svg"></img></a>
+            <a href="https://twitter.com/mjwsteenbergen/"><img src="/icons/logo-twitter.svg"></img></a>
+          </div>
+        </div>
       </div>
       
     </Layout>
