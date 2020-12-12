9f53cbf27c2d7a99ea942224bccc761b8fbdcff3
Martijn Steenbergen <m.uses+git@outlook.com>
2020-12-12 22:47:08 +0100

    Open career links in new tab

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -44,13 +44,12 @@ const StaticPropsDetail = ({ item, errors }: Props) => {
         <div className="about-box-box">
           <p>Hi! I'm Martijn Steenbergen. I am a 25 year old student living in Delft, where I have just finished my masters in computer science. I have traveled the world, am fascinated with UI/UX and love TV-series. </p>
           <div className="icon-container">
-            <a href="https://github.com/mjwsteenbergen/"><img src="/icons/logo-github.svg"></img></a>
-            <a href="https://www.linkedin.com/in/mjwsteenbergen/"><img src="/icons/logo-linkedin.svg"></img></a>
-            <a href="https://twitter.com/mjwsteenbergen/"><img src="/icons/logo-twitter.svg"></img></a>
+            <a target="blank" href="https://github.com/mjwsteenbergen/"><img src="/icons/logo-github.svg"></img></a>
+            <a target="blank" href="https://www.linkedin.com/in/mjwsteenbergen/"><img src="/icons/logo-linkedin.svg"></img></a>
+            <a target="blank" href="https://twitter.com/mjwsteenbergen/"><img src="/icons/logo-twitter.svg"></img></a>
           </div>
         </div>
       </div>
-      
     </Layout>
   )
 }
