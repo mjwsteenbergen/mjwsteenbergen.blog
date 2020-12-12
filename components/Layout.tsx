54fd273dd0401b8ac4e79b4f07382f6db676a3c3
Martijn Steenbergen <m.uses+git@outlook.com>
2020-12-13 00:44:05 +0100

    fix tag manager

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -20,7 +20,7 @@ const Layout = (props: Props) => {
   }
   const code = `
   window.dataLayer = window.dataLayer || [];
-  function gtag(){"{dataLayer.push(arguments);}"}
+  function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'G-GTLDZDRMZH');
 `;
