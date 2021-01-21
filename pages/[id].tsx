74e369717d2fda677bac7f782d2b112b7727ff16
Martijn Steenbergen <m.uses+git@outlook.com>
2021-01-21 16:51:51 +0100

    Convert post to react component

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps, GetStaticPaths } from 'next'
+import React from 'react'
 import Layout from '../components/Layout'
 import TableOfContents from '../components/TableOfContents'
 import { BlogPosts, BlogPost } from '../utils/laurentia'
@@ -12,54 +13,43 @@ export const config = {
   unstable_runtimeJS: false
 }
 
-const StaticPropsDetail = ({ item, errors }: Props) => {
-  if (errors) {
+export default class IndexPage extends React.Component<Props,{}> {
+  render() {
+    const item = this.props.item;
+    let image = item?.coverImage ? <img src={item?.coverImage} className="coverimage"></img> : "";
+
     return (
-      <Layout title="Error | Next.js + TypeScript Example">
-        <p>
-          <span style={{ color: 'red' }}>Error:</span> {errors}
-        </p>
-      </Layout>
-    )
-  }
-
-
-  let image = item?.coverImage ? <img src={item?.coverImage} className="coverimage"></img> : "";
-
-  return (
-    <Layout
-      title={item?.name}
-      description={item?.shortDescription}
-    >
-      {image}
-      <div className="article">
-        <p className="back-to-main">By Martijn Steenbergen</p>
-        <h1 className="article-title">{item?.name}</h1>
-        <TableOfContents/>
-        <article dangerouslySetInnerHTML={{ __html: item?.html || "" }} ></article>
-      </div>
-      <a href="/"> 
-        <div className="back-to-main-page">
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><title>Arrow Back</title><path fill='none' stroke='currentColor' stroke-linecap='square' stroke-miterlimit='10' stroke-width='48' d='M244 400L100 256l144-144M120 256h292' /></svg>
-          <p>View all posts</p>
+      <Layout
+        title={item?.name}
+        description={item?.shortDescription}
+      >
+        {image}
+        <div className="article">
+          <p className="back-to-main">By Martijn Steenbergen</p>
+          <h1 className="article-title">{item?.name}</h1>
+          <TableOfContents />
+          <article dangerouslySetInnerHTML={{ __html: item?.html || "" }} ></article>
         </div>
-      </a>
-      <div className="about-box">
-        <img src="https://avatars0.githubusercontent.com/u/5946409"></img>
-        <div className="about-box-box">
-          <p>Hi! I'm Martijn Steenbergen. I am a 25 year old student living in Delft, where I have just finished my masters in computer science. I have traveled the world, am fascinated with UI/UX and love TV-series. </p>
-          <div className="icon-container">
-            <a target="blank" href="https://github.com/mjwsteenbergen/"><img src="/icons/logo-github.svg"></img></a>
-            <a target="blank" href="https://www.linkedin.com/in/mjwsteenbergen/"><img src="/icons/logo-linkedin.svg"></img></a>
-            <a target="blank" href="https://twitter.com/mjwsteenbergen/"><img src="/icons/logo-twitter.svg"></img></a>
+        <a href="/">
+          <div className="back-to-main-page">
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><title>Arrow Back</title><path fill='none' stroke='currentColor' stroke-linecap='square' stroke-miterlimit='10' stroke-width='48' d='M244 400L100 256l144-144M120 256h292' /></svg>
+            <p>View all posts</p>
+          </div>
+        </a>
+        <div className="about-box">
+          <img src="https://avatars0.githubusercontent.com/u/5946409"></img>
+          <div className="about-box-box">
+            <p>Hi! I'm Martijn Steenbergen. I am a 25 year old student living in Delft, where I have just finished my masters in computer science. I have traveled the world, am fascinated with UI/UX and love TV-series. </p>
+            <div className="icon-container">
+              <a target="blank" href="https://github.com/mjwsteenbergen/"><img src="/icons/logo-github.svg"></img></a>
+              <a target="blank" href="https://www.linkedin.com/in/mjwsteenbergen/"><img src="/icons/logo-linkedin.svg"></img></a>
+              <a target="blank" href="https://twitter.com/mjwsteenbergen/"><img src="/icons/logo-twitter.svg"></img></a>
+            </div>
           </div>
         </div>
-      </div>
-    </Layout>
-  )
+      </Layout>);
+  }
 }
-
-export default StaticPropsDetail
 
 export const getStaticPaths: GetStaticPaths = async () => {
   // Get the paths we want to pre-render based on blogposts
